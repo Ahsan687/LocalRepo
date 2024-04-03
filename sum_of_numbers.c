d1f2b017420f920f37b302215990410a2bd4ae8f
ahsan <ahsanulhaqreshi@gmail.com>
2024-04-03 16:01:13 +0530

    update

---
+++ sum_of_numbers.c
@@ -0,0 +1,21 @@
+/* programe_3 */ 
+/* author Ahsan ul haq */
+/* roll number 22069120033 */
+/*simple intrest calculation*/
+
+#include <stdio.h>
+int main()
+{
+    float x, y, z ;      /*this is decleration statement*/
+        printf("enter the ist number");
+        scanf("%f",&x);
+        printf("enter the 2nd number");
+        scanf("%f", &y);
+
+        z = (x + y);
+
+        printf("sum is = %f" , z);
+
+    return 0;
+
+}
